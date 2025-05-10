16a4da0dea401c3c1c8c077e3ee9898d527bafdc
Andrés Christian Meneguzzo <andres.meneguzzo@gmail.com>
2025-05-10 22:43:41 +0200

    added the protected route /me so only authorized user can access user data

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const {registerUser, loginUser} = require('../controllers/userController');
+const {registerUser, loginUser, getMe} = require('../controllers/userController');
+const {protect} = require('../middleware/authMiddleware');
+
 
 // Register route
 router.post('/', registerUser);
 
 // Login route
 router.post('/login', loginUser);
+
+// protected routes
+router.get('/me', protect, getMe); // protect the route with the auth middleware, so only authorized users can access the profile with user data
 
 module.exports = router;
